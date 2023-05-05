ddbab247d9633a44d7f2f4df4d09e16ab91ccb38
JABAGAMEHUNTER <jmatheo71@gmail.com>
2023-05-04 22:18:14 -0400

    adicionadas rotas para autenticacao com github

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,19 @@
 
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/users', [UserController::class, 'index']);
 Route::post('/users', [UserController::class, 'store']);
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('github')->redirect();
+});
+
+Route::get('/auth/callback', function (){
+    $user = Socialite::driver('github')->user();
+});
 
 Route::get('/', function() {
         return response()->json([
